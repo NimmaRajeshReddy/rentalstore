a7179c95e66e6dc9042b348c14678163912fa0d2
NimmaRajeshReddy <npsreddy98@gmail.com>
2020-06-03 22:59:36 +0530

    php files

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p style="font-size:16px;" align="center">Copyright © Online Rental Store. All Rights Reserved | Contact Us: +91 90000 00000</p>
+    </div>
+</footer>
